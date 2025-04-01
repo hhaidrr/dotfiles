1f64bb0c9dd8bc83efb5e08085a4b6bd70a6c21f
Hamzah Haider <hamzahhaider1@gmail.com>
2025-04-01 09:24:50 -0400

    feat: setup script updated

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hamzah Haider Dotfiles
+
+## Dependencies
+- [Git](https://git-scm.com/)
+- [Zsh](https://www.zsh.org/)
+- [Oh-My-Zsh](https://ohmyz.sh/)
+- [GNU Stow](https://www.gnu.org/software/stow/)
+- [Neovim](https://neovim.io/)
+- [Starship](https://starship.rs/)
--- setup.sh
+++ setup.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
 if command -v apt-get &> /dev/null; then
-    apt-get install stow
+    sudo apt-get update
+    sudo apt-get install -y \
+        stow \
+        zsh \
+
+    sudo apt autoremove -y
+    sudo apt clean
+
 else
     echo "APT is not available on $(uname -s). Please install dependencies manually."
+    exit 1
+fi
 
-stow .
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    # Install Oh My Zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if ! command -v brew &> /dev/null; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install Homebrew packages
+~/linuxbrew/.linuxbrew/bin/brew bundle --file=./.config/homebrew/Brewfile
+
+# Initialize Symlinks
+/usr/bin/stow .
+
 
