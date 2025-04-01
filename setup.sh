82a3d4bca03cc9726d086c87fdb4c1e738b5da3b
Hamzah Haider <hamzahhaider1@gmail.com>
2025-04-01 09:37:27 -0400

    fix: homebrew install noninteractive

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ fi
 
 if ! command -v brew &> /dev/null; then
     echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Homebrew is already installed."
 fi
